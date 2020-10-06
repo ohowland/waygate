873aa414699787857f82f5325b5dbec0b18c49b3
vagrant <vagrant@vagrant.vm>
2020-10-06 00:49:13 +0000

    update

---
+++ src/waygate.hpp
@@ -0,0 +1,6 @@
+#define WAYGATE_VERSION_MAJOR @WAYGATE_VERSION_MAJOR@
+#define WAYGATE_VERSION_MINOR @WAYGATE_VERSION_MINOR@
+#define PROJECT_CONFIG_DIR @PROJECT_CONFIG_DIR@
+
+#include "reader.hpp"
+#include "signals.hpp"
